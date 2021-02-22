3dec7c97e94776965af4be47dbde43dbf4f7e947
Michael Mannucci <admin@eklektos.io>
2021-02-22 16:38:14 -0500

    Spaces

--- config/filesystems.php
+++ config/filesystems.php
@@ -65,6 +65,15 @@ return [
             'endpoint' => env('AWS_ENDPOINT'),
         ],
 
+        'spaces' => [
+            'driver' => 's3',
+            'key' => env('DO_SPACES_KEY'),
+            'secret' => env('DO_SPACES_SECRET'),
+            'region' => env('DO_SPACES_REGION'),
+            'bucket' => env('DO_SPACES_BUCKET'),
+            'endpoint' => env('DO_SPACES_ENDPOINT'),
+        ],
+
         'images' => [
             'driver' => 'local',
             'root' => public_path('assets/images'),
