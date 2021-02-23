93472ad63c0dd5740f00b6419f65e160b94ea347
Michael Mannucci <admin@eklektos.io>
2021-02-22 22:52:02 -0500

    filesystems

--- config/filesystems.php
+++ config/filesystems.php
@@ -72,6 +72,7 @@ return [
             'region' => env('DO_SPACES_REGION'),
             'bucket' => env('DO_SPACES_BUCKET'),
             'endpoint' => env('DO_SPACES_ENDPOINT'),
+            'visibility' => 'public',
         ],
 
         'images' => [
